c956f08c428fb6142430d977b678cbdc53ebc6f3
Pushpenderrathore <pushpendersingh046@gmail.com>
2025-09-25 06:15:06 -0400

    Antares in Development phase Cautiongit add .

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Copy AppImage to /usr/local/bin and rename it
+sudo cp eDEX-UI-Linux-x86_64.AppImage /usr/local/bin/antares
+
+# Make it executable
+sudo chmod +x /usr/local/bin/antares
+
+
+
